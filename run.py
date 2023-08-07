52928916ea8930f9dc8d57497f4f0eaf82ea3596
Blaize-Egelhof <>
2023-08-07 17:01:31 +0000

    feat! create file to store hangman visuals in a list + create function to show rules of game + moved all functions into a main function + created function to show hangman and random word to user

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ import random
 import time
 #Pull the list variable containing all my words
 from random_words import random_word_list 
+#Pull the list containing the visuals for hangman
+from hangman_visuals import stages
 
 #Global variable to keep track of the users guess attempts
 user_attempts = 6
@@ -41,7 +43,8 @@ def rules_of_the_game(users_name):
     '''
     print(f"Some Rules of this HangMan Game:\n 1)You will Get 6 attempts to guess the word. \n 2)If you fail to guess the word within 7 tries you have caused Mr Hangmans death and will have to try again \n 3)If you sucessfully guessed the word you will have saved Mr HangMans life and won the round! \n Thank you for playing my game,{users_name} please enjoy!")
     time.sleep(3)
-
+    print("--------------------------------------------------------------------------------------------------------------------------------------------------")
+    print("--------------------------------------------------------------------------------------------------------------------------------------------------")
 
 
 def main():
@@ -50,88 +53,35 @@ def main():
     valid_name = check_users_inputted_name()
     print(f"Hello {valid_name}!")
     rules_of_the_game(valid_name)
+    show_word_and_hangman(random_word)
 
 
 
-def display_hangman():
-
+def show_word_and_hangman(random_word): 
     '''
-    Function to visually show current HangMan status based on users attempts, this function was taken , then modifed from https://github.com/kiteco/python-youtube-code/blob/master/build-hangman-in-python/hangman.py
+    Function to take the randomly chosen word as a parameter and convert it to a # string to hide the current word from user + display HangMans Status to user
     '''
-   stages = [
-        """
-          +---+
-              |
-              |
-              |
-              |
-              |
-        ==========
-        """,
-        """
-          +---+
-          |   |
-              |
-              |
-              |
-              |
-        ==========
-        """,
-        """
-          +---+
-          |   |
-          O   |
-              |
-              |
-              |
-        ==========
-        """,
-        """
-          +---+
-          |   |
-          O   |
-          |   |
-              |
-              |
-        ==========
-        """,
-        """
-          +---+
-          |   |
-          O   |
-         /|   |
-              |
-              |
-        ==========
-        """,
-        """
-          +---+
-          |   |
-          O   |
-         /|\\  |
-              |
-              |
-        ==========
-        """,
-        """
-          +---+
-          |   |
-          O   |
-         /|\\  |
-         /    |
-              |
-        ==========
-        """,
-        """
-          +---+
-          |   |
-          O   |
-         /|\\  |
-         / \\  |
-              |
-        ==========
-        """
-    ]
+    print(stages[0])
+    word_place_holder = ["#" for _ in random_word]
+    print(f"The length of this word is:{len(random_word)}")
+    print(word_place_holder)
+    check_users_inputted_answers()
+    
+
+
+
+def check_users_inputted_answers():
+
+    while True: 
+    user_name = input("Please enter your name here: ")
+    if len(user_name) == 0 or any(char.isdigit() or char.isspace() for char in user_name):
+        print(f"Invalid name, please re-enter your name without any numbers or blank spaces, you entered: {user_name}")
+    else:
+        print("Valid name, proceding...")
+        return user_name
+
+
+
 
 
 main()
