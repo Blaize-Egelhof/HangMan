737a304c2c4df53405c0ee827b5575fdc69d9dbf
Blaize-Egelhof <>
2023-08-17 11:07:29 +0000

    style ! Passed hangman_visuals through PEP8 and corrected any errors

--- hangman_visuals.py
+++ hangman_visuals.py
@@ -1,3 +1,8 @@
+'''
+Visually show current HangMan status based on users attempts,
+These image examples was taken from Kiteco ,then modifed, credits below:
+https://github.com/kiteco/python-youtube-code/blob/master/build-hangman-in-python/hangman.py
+'''
 stages = [
         """
           +---+
